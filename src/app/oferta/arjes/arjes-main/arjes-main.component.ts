89c9835a7c175fc40f8a97613e9c704f6a52e2be
Bartosz Prochowski <bravoprochu@gmail.com>
2020-02-21 01:31:06 +0100

    arjes oferta add small/big text column

--- src/app/oferta/arjes/arjes-main/arjes-main.component.ts
+++ src/app/oferta/arjes/arjes-main/arjes-main.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CardPersonService } from 'src/app/common/card-person/card-person.service';
 import { ICardPerson } from 'src/app/common/interfaces/i-card-person';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-arjes-main',
@@ -10,14 +13,45 @@ import { ICardPerson } from 'src/app/common/interfaces/i-card-person';
 export class ArjesMainComponent implements OnInit {
 
   constructor(
-    private cardPersonService: CardPersonService
+    private cardPersonService: CardPersonService,
+    private breakpointObs: BreakpointObserver
   ) { }
 
+  isDestroyed$: Subject<boolean> = new Subject()
+  cssMainTextCols: string;
+  contactCards: ICardPerson[];
 
-  contactCards: ICardPerson[] = [];
+  ngOnDestroy(): void {
+    this.isDestroyed$.next(true);
+    this.isDestroyed$.complete();
+    this.isDestroyed$.unsubscribe();
+  }
 
   ngOnInit() {
+    const _LANDSCAPE = [Breakpoints.HandsetLandscape, Breakpoints.TabletLandscape, Breakpoints.WebLandscape];
+    const _PORTRAIT = [Breakpoints.HandsetPortrait, Breakpoints.TabletPortrait, Breakpoints.WebPortrait];
+    const _SMALL = [Breakpoints.HandsetPortrait, Breakpoints.HandsetLandscape, Breakpoints.TabletPortrait, Breakpoints.TabletLandscape];
+    const _REST = [Breakpoints.XSmall, Breakpoints.Small, Breakpoints.Medium, Breakpoints.Large, Breakpoints.XLarge];
 
+    this.breakpointObs.observe([..._REST]).pipe(
+      takeUntil(this.isDestroyed$)
+    )
+      .subscribe(
+        (_breakpObs: any) => {
+          this.cssMainTextCols = 'oferta-main-text-col-2';
+
+          if (this.breakpointObs.isMatched(_SMALL)) {
+            this.cssMainTextCols = this.breakpointObs.isMatched(_PORTRAIT) ? '' : 'oferta-main-text-col-2'
+          }
+        },
+        (error) => console.log('_breakpObs error', error),
+        () => console.log('_breakpObs completed..')
+      );
+
+    
+
+
+    this.contactCards = [];
     this.contactCards.push(this.cardPersonService.getDelaerByNameSlashMark('radlak/arjes'));
 
   }
